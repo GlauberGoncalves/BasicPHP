6a27d04184e1fd6620c7d0b6a03553544887d59d
Glauber <glauber_g3@yahoo.com.br>
2016-04-29 19:20:50 +0000

    first Commit

---
+++ variavel.php
@@ -0,0 +1,17 @@
+<?php
+// Uso de variaveis em php
+
+$nome = "Wolverine ";
+$sobrenome = "dos X-men";
+
+    echo "Usando variaveis do tipo string <br><Br>";
+    echo "$nome $sobrenome<Br><br>";
+    
+    echo "Usando variaveis do tipo inteiro<br><Br>";
+        $AnoDeNascimento = 1988;
+        $MesDeNascimento = 04;
+        $DiaDeNascimento = 30;
+        
+    echo "Nascido em $DiaDeNascimento/$MesDeNascimento/$AnoDeNascimento"; 
+    
+?>
